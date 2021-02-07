c097e628aad86eb87049755865e1e75aff3022c1
Andrew <andru85@mail.ru>
2021-02-07 20:18:46 +0500

    добавлен конструктор копирования

--- include/Users.h
+++ include/Users.h
@@ -6,20 +6,35 @@
 using namespace std;
 
 class Users {
-  public:
-    Users();
+
+public:
+    Users(string nickname, string password,double rating);
 
     virtual ~Users();
 
+    void setnickname(string);
+    string getnickname();
+
+    void setpassword(string);
+    string getpassword();
+
+    void setrating(double);
+    double getrating();
+
     Users(Users & source);
 
-    Users(const Users & source);
+    /*Users(const Users & source);
 
     Users & operator=(Users & source);
 
     Users & operator=(const Users & source);
 
-    friend  ostream & operator<<(ostream & os, const Users & x);
+    friend  ostream & operator<<(ostream & os, const Users & x);*/
+
+private:
+    string _nickname;
+    string _password;
+    double _rating;
 
 };
 #endif
